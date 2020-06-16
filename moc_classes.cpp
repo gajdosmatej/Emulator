0d4f492dccf2984ab23439cf2d541da905ce3c04
Thrundus <thrundus@localhost.localdomain>
2020-06-16 13:42:06 +0200

    [FIX] Nefunkcni kompilace

--- moc_classes.cpp
+++ moc_classes.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'classes.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -13,7 +13,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'classes.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.14.1. It"
+#error "This file was generated using the moc from 5.14.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -92,8 +92,8 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     return _id;
 }
 struct qt_meta_stringdata_Editor_t {
-    QByteArrayData data[1];
-    char stringdata0[7];
+    QByteArrayData data[3];
+    char stringdata0[14];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -102,10 +102,12 @@ struct qt_meta_stringdata_Editor_t {
     )
 static const qt_meta_stringdata_Editor_t qt_meta_stringdata_Editor = {
     {
-QT_MOC_LITERAL(0, 0, 6) // "Editor"
+QT_MOC_LITERAL(0, 0, 6), // "Editor"
+QT_MOC_LITERAL(1, 7, 5), // "clear"
+QT_MOC_LITERAL(2, 13, 0) // ""
 
     },
-    "Editor"
+    "Editor\0clear\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -115,21 +117,32 @@ static const uint qt_meta_data_Editor[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   19,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+
        0        // eod
 };
 
 void Editor::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Editor *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->clear(); break;
+        default: ;
+        }
+    }
     Q_UNUSED(_a);
 }
 
@@ -159,6 +172,17 @@ void *Editor::qt_metacast(const char *_clname)
 int Editor::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
     return _id;
 }
 struct qt_meta_stringdata_CompileButton_t {
@@ -458,27 +482,27 @@ int Head::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     }
     return _id;
 }
-struct qt_meta_stringdata_Controller_t {
+struct qt_meta_stringdata_ErrorDevice_t {
     QByteArrayData data[3];
-    char stringdata0[21];
+    char stringdata0[23];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_Controller_t, stringdata0) + ofs \
+    qptrdiff(offsetof(qt_meta_stringdata_ErrorDevice_t, stringdata0) + ofs \
         - idx * sizeof(QByteArrayData)) \
     )
-static const qt_meta_stringdata_Controller_t qt_meta_stringdata_Controller = {
+static const qt_meta_stringdata_ErrorDevice_t qt_meta_stringdata_ErrorDevice = {
     {
-QT_MOC_LITERAL(0, 0, 10), // "Controller"
-QT_MOC_LITERAL(1, 11, 8), // "loadText"
-QT_MOC_LITERAL(2, 20, 0) // ""
+QT_MOC_LITERAL(0, 0, 11), // "ErrorDevice"
+QT_MOC_LITERAL(1, 12, 9), // "readState"
+QT_MOC_LITERAL(2, 22, 0) // ""
 
     },
-    "Controller\0loadText\0"
+    "ErrorDevice\0readState\0"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_Controller[] = {
+static const uint qt_meta_data_ErrorDevice[] = {
 
  // content:
        8,       // revision
@@ -492,10 +516,112 @@ static const uint qt_meta_data_Controller[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x0a /* Public */,
+       1,    0,   19,    2, 0x09 /* Protected */,
 
  // slots: parameters
     QMetaType::Void,
+
+       0        // eod
+};
+
+void ErrorDevice::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<ErrorDevice *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->readState(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject ErrorDevice::staticMetaObject = { {
+    QMetaObject::SuperData::link<Device::staticMetaObject>(),
+    qt_meta_stringdata_ErrorDevice.data,
+    qt_meta_data_ErrorDevice,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *ErrorDevice::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ErrorDevice::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ErrorDevice.stringdata0))
+        return static_cast<void*>(this);
+    return Device::qt_metacast(_clname);
+}
+
+int ErrorDevice::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = Device::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+    return _id;
+}
+struct qt_meta_stringdata_Controller_t {
+    QByteArrayData data[8];
+    char stringdata0[82];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Controller_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Controller_t qt_meta_stringdata_Controller = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "Controller"
+QT_MOC_LITERAL(1, 11, 8), // "loadText"
+QT_MOC_LITERAL(2, 20, 0), // ""
+QT_MOC_LITERAL(3, 21, 9), // "Compiler*"
+QT_MOC_LITERAL(4, 31, 8), // "compiler"
+QT_MOC_LITERAL(5, 40, 12), // "ProcessLoop*"
+QT_MOC_LITERAL(6, 53, 11), // "processLoop"
+QT_MOC_LITERAL(7, 65, 16) // "getNumberOfPorts"
+
+    },
+    "Controller\0loadText\0\0Compiler*\0compiler\0"
+    "ProcessLoop*\0processLoop\0getNumberOfPorts"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Controller[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    2,   24,    2, 0x0a /* Public */,
+       7,    0,   29,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3, 0x80000000 | 5,    4,    6,
+    QMetaType::Int,
 
        0        // eod
 };
@@ -506,11 +632,23 @@ void Controller::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         auto *_t = static_cast<Controller *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->loadText(); break;
+        case 0: _t->loadText((*reinterpret_cast< Compiler*(*)>(_a[1])),(*reinterpret_cast< ProcessLoop*(*)>(_a[2]))); break;
+        case 1: { int _r = _t->getNumberOfPorts();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
         default: ;
         }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 0:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 1:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< ProcessLoop* >(); break;
+            }
+            break;
+        }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject Controller::staticMetaObject = { {
@@ -542,6 +680,207 @@ int Controller::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
+    return _id;
+}
+struct qt_meta_stringdata_DeviceManager_t {
+    QByteArrayData data[5];
+    char stringdata0[44];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_DeviceManager_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_DeviceManager_t qt_meta_stringdata_DeviceManager = {
+    {
+QT_MOC_LITERAL(0, 0, 13), // "DeviceManager"
+QT_MOC_LITERAL(1, 14, 7), // "proceed"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 7), // "Editor*"
+QT_MOC_LITERAL(4, 31, 12) // "deviceEditor"
+
+    },
+    "DeviceManager\0proceed\0\0Editor*\0"
+    "deviceEditor"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_DeviceManager[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   19,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+
+       0        // eod
+};
+
+void DeviceManager::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<DeviceManager *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->proceed((*reinterpret_cast< Editor*(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 0:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< Editor* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject DeviceManager::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_DeviceManager.data,
+    qt_meta_data_DeviceManager,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *DeviceManager::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *DeviceManager::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_DeviceManager.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int DeviceManager::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    }
+    return _id;
+}
+struct qt_meta_stringdata_Queue_t {
+    QByteArrayData data[3];
+    char stringdata0[17];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Queue_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Queue_t qt_meta_stringdata_Queue = {
+    {
+QT_MOC_LITERAL(0, 0, 5), // "Queue"
+QT_MOC_LITERAL(1, 6, 9), // "callQueue"
+QT_MOC_LITERAL(2, 16, 0) // ""
+
+    },
+    "Queue\0callQueue\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Queue[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   19,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Queue::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Queue *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->callQueue(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject Queue::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_Queue.data,
+    qt_meta_data_Queue,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Queue::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Queue::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Queue.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Queue::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
         if (_id < 1)
             qt_static_metacall(this, _c, _id, _a);
         _id -= 1;
@@ -549,6 +888,104 @@ int Controller::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         if (_id < 1)
             *reinterpret_cast<int*>(_a[0]) = -1;
         _id -= 1;
+    }
+    return _id;
+}
+struct qt_meta_stringdata_ProcessLoop_t {
+    QByteArrayData data[4];
+    char stringdata0[24];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_ProcessLoop_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_ProcessLoop_t qt_meta_stringdata_ProcessLoop = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "ProcessLoop"
+QT_MOC_LITERAL(1, 12, 4), // "stop"
+QT_MOC_LITERAL(2, 17, 0), // ""
+QT_MOC_LITERAL(3, 18, 5) // "cycle"
+
+    },
+    "ProcessLoop\0stop\0\0cycle"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ProcessLoop[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x0a /* Public */,
+       3,    0,   25,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void ProcessLoop::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<ProcessLoop *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->stop(); break;
+        case 1: _t->cycle(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject ProcessLoop::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_ProcessLoop.data,
+    qt_meta_data_ProcessLoop,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *ProcessLoop::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ProcessLoop::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ProcessLoop.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int ProcessLoop::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
     }
     return _id;
 }
